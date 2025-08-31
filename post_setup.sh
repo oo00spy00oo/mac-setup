4761be142f56d23d914ef6fea0a8e7b40f2b87b0
Spy Kab <ooo000spy000ooo@gmail.com>
2025-09-01 01:19:48 +0700

    Create post_setup.sh for environment configuration

---
+++ post_setup.sh
@@ -0,0 +1,10 @@
+# LazyVim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+# Volta
+curl https://get.volta.sh | bash
+volta install node
+volta install pnpm
+
+# Chezmoi
+chezmoi init git@github.com:oo00spy00oo/dotfiles.git
